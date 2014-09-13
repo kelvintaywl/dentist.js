a6b4df3ecafc87bcf59af57c157a62d6193a43f2
kelvintaywl <kelvintaywl@gmail.com>
2014-09-13 16:47:17 +0800

    clean up, rewrite main JS

--- js/dentist.js
+++ js/dentist.js
@@ -1,37 +1,31 @@
 !(function(window, document, undefined){
-	window.extract = function extract(str_to_split, opts){
-		var opts = opts || {},
-			STR = str_to_split || document.URL,
-			SETTINGS = {
-				start: opts.start && typeof opts.start === 'number'? opts.start : opts.start && typeof opts.start === 'string'? STR.indexOf(opts.start) : STR.indexOf('?') + 1,
-				stop: opts.stop && typeof opts.stop === 'number'? opts.stop : opts.stop && typeof opts.stop === 'string'? STR.indexOf(opts.stop) : STR.length,
-				param_delim : opts.param_delim || '&',
-				kv_delim : opts.kv_delim || '=',
-				keys : (opts.keys && Object.prototype.toString.call(opts.keys) === '[object Array]')? opts.keys : opts.keys? opts.keys : null,
-			};
+	var DEFAULTS = {
+		delimiter: '&',
+		key_value_separator: '=',
+		params_prefix: '?',
+	};
 
-		try {
-			var results = {}, 
-				requested_results = {}, 
-				substr = STR.substring(SETTINGS.start, SETTINGS.stop);
-			if(substr.length<1){ return null; };
-			
-			var params = substr.split(SETTINGS.param_delim);
+	String.prototype.extract = function(opts){
+		var opts = opts || {},
+			keyValuePairs = [],
+			params = {};
+
+		opts.delimiter = opts.delimiter || DEFAULTS.delimiter;
+		opts.key_value_separator = opts.key_value_separator || DEFAULTS.key_value_separator;
+		opts.params_prefix = opts.params_prefix || DEFAULTS.params_prefix;
+
+		var indexParamsPrefix = this.indexOf(opts.params_prefix)
+
+		// scope of finding params only applicable to str
+		var str = indexParamsPrefix < 0? this : this.substring(indexParamsPrefix+1); 
 		
-			for(var i=0; i<params.length; i++){
-				var kv_pair = params[i].split(SETTINGS.kv_delim);
-				results[kv_pair[0]] = kv_pair[1];
-			};
-			if(!SETTINGS.keys){
-				return Object.keys(results).length > 1? results : results[Object.keys(results)[0]];
-			};
-			for(var i=0; i< SETTINGS.keys.length; i++){
-				requested_results[SETTINGS.keys[i]] = results[SETTINGS.keys[i]] || null;
-			};
-			return Object.keys(requested_results).length > 1? requested_results : requested_results[keys[0]];
-		}
-		catch (e) {
-	  	  	return;
-		};	
+		keyValuePairs = str.split(opts.delimiter);
+		var kv_pair;
+		for(var i=0, s=keyValuePairs.length; i<s; i++){
+			kv_pair = keyValuePairs[i].split(opts.key_value_separator);
+			// ignore any items after first value found, where key = kv_pair[0], value = kv_pair[1]
+			params[kv_pair[0]] = kv_pair[1];
+		};
+		return params;
 	};
 })(window, document);
